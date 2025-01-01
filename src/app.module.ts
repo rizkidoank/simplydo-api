4f09370ffb8b39de92c7b02585aa8be91c488635
Rizki <rizki@rizkidoank.com>
2025-01-01 16:29:14 +0700

    add configuration to the project and its base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASS'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.service.spec.ts
@@ -0,0 +1,39 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { join } from 'path';
+
+describe('ConfigService', () => {
+  let configService: ConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: join(__dirname, '../../.env.test'),
+          isGlobal: true,
+        }),
+      ],
+      providers: [ConfigService],
+    }).compile();
+
+    configService = module.get<ConfigService>(ConfigService);
+  });
+
+  it('should be defined', () => {
+    expect(configService).toBeDefined();
+    console.log(configService);
+  });
+
+  it('should return correct DATABASE_HOST string', () => {
+    const databaseHost = configService.get<string>('DATABASE_HOST');
+    expect(databaseHost).toBe('localhost');
+  });
+
+  it('should return correct DATABASE_PORT number', () => {
+    expect(Number(configService.get<number>('DATABASE_PORT'))).toBe(5432);
+  });
+
+  it('should return undefined for undefined key', () => {
+    expect(configService.get<string>('RANDOM_KEY')).toBeUndefined();
+  });
+});
