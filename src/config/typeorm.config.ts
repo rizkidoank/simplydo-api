42b1b2450164ff499f53b4f24ef4de9c872e3904
Rizki <rizki@rizkidoank.com>
2025-01-03 14:17:32 +0700

    create migrations and disable synchronize

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+config();
+
+const configService = new ConfigService();
+
+console.log('DATABASE_HOST:', configService.get<string>('DATABASE_HOST'));
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('DATABASE_HOST'),
+  port: Number(configService.get<string>('DATABASE_PORT')),
+  username: configService.get<string>('DATABASE_USER'),
+  password: configService.get<string>('DATABASE_PASS'),
+  database: configService.get<string>('DATABASE_NAME'),
+  synchronize: false,
+  entities: ['**/*.entity.ts'],
+  migrations: ['src/database/migrations/*-migration.ts'],
+  migrationsRun: false,
+  logging: true,
+});
+
+export default AppDataSource;
